6b1c137b85c353de064e0e9d1f6670b8845a173b
ZTHLLY <zzz18011940139@gmail.com>
2025-08-25 18:22:42 +1000

    my change

--- server/main.go
+++ server/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type PageData struct {
 	Projects []Project
-	Tasks []Task
+	Tasks    []Task
 	ActiveId string
 }
 
@@ -22,14 +22,16 @@ func internalErr(w http.ResponseWriter, err error) {
 }
 
 func main() {
+	fmt.Println("POSTGRES_URL:", os.Getenv("POSTGRES_URL"))
 	dbpool, err := pgxpool.New(context.Background(), os.Getenv("POSTGRES_URL"))
+	//dbpool, err := pgxpool.New(context.Background(), "postgres://postgres:admin@localhost:5432/duit?sslmode=disable")
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	defer dbpool.Close()
 
 	mux := http.NewServeMux()
-	tmpl := template.Must(template.ParseFiles("client/index.html"))
+	tmpl := template.Must(template.ParseFiles("../client/index.html"))
 
 	mux.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
@@ -62,7 +64,25 @@ func main() {
 		w.Write(response) // todo: handle err
 	})
 
-	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("client"))))
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../client"))))
 
+	mux.HandleFunc("POST /addTask", func(w http.ResponseWriter, r *http.Request) {
+		var req map[string]interface{}
+		//err =json.Unmarshal(r.Body, req)
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		task := req["task"].(string)
+		project := req["project"].(string)
+
+		fmt.Println("task", task)
+		fmt.Println("project", project)
+
+		w.Write([]byte("Task is" + task))
+
+	})
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
